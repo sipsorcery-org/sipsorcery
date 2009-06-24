9e6963b0c2d165e67df2c336c6e316ce0bf5c070
aaronc_cp <SND\aaronc_cp>
2009-06-24 02:58:14 -0700

    Added interface for SIPCallManager.

---
+++ sipsorcery-core/SIPSorcery.SIP.App/SIPAssets/ISIPCallManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIPSorcery.SIP.App {
+    public interface ISIPCallManager {
+        string ProcessWebCallback(string username, string number);
+        void CreateDialogueBridge(SIPDialogue firstLegDialogue, SIPDialogue secondLegDialogue, string owner);
+        void ReInvite(SIPDialogue firstLegDialogue, string RemoteSDP);
+        int GetCurrentCallCount(string owner);
+    }
+}
