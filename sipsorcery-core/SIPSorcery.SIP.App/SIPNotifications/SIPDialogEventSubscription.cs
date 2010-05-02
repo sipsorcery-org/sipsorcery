e3c6da2e023b219abdf26efb656133bdda7e1c31
aaronc_cp <SND\aaronc_cp>
2010-05-02 05:46:15 -0700

    Added missing SIP notification files.

---
+++ sipsorcery-core/SIPSorcery.SIP.App/SIPNotifications/SIPDialogEventSubscription.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SIPSorcery.Persistence;
+
+namespace SIPSorcery.SIP.App
+{
+    public class SIPDialogEventSubscription : SIPEventSubscription
+    {
+        private const int MAX_DIALOGUES_FOR_NOTIFY = 25;
+
+        private static string m_contentType = SIPMIMETypes.DIALOG_INFO_CONTENT_TYPE;
+
+        private SIPEventDialogInfo DialogInfo;
+
+        private SIPAssetGetListDelegate<SIPDialogueAsset> GetDialogues_External;
+        private SIPAssetGetByIdDelegate<SIPDialogueAsset> GetDialogue_External;
+
+        public override SIPEventPackage SubscriptionEventPackage
+        {
+            get { return SIPEventPackage.Dialog; }
+        }
+
+        public override string MonitorFilter
+        {
+            get { return "dialog " + ResourceURI.ToString(); }
+        }
+
+        public override string NotifyContentType
+        {
+            get { return m_contentType; }
+        }
+
+        public SIPDialogEventSubscription(
+            SIPMonitorLogDelegate log,
+            string sessionID,
+            SIPURI resourceURI,
+            SIPURI canonincalResourceURI,
+            string filter,
+            SIPDialogue subscriptionDialogue,
+            int expiry,
+            SIPAssetGetListDelegate<SIPDialogueAsset> getDialogues,
+            SIPAssetGetByIdDelegate<SIPDialogueAsset> getDialogue
+            )
+            : base(log, sessionID, resourceURI, canonincalResourceURI, filter, subscriptionDialogue, expiry)
+        {
+            GetDialogues_External = getDialogues;
+            GetDialogue_External = getDialogue;
+            DialogInfo = new SIPEventDialogInfo(0, SIPEventDialogInfoStateEnum.full, resourceURI);
+        }
+
+        public override void GetFullState()
+        {
+            try
+            {
+                DialogInfo.State = SIPEventDialogInfoStateEnum.full;
+                List<SIPDialogueAsset> dialogueAssets = GetDialogues_External(d => d.Owner == SubscriptionDialogue.Owner, "Inserted", 0, MAX_DIALOGUES_FOR_NOTIFY);
+
+                foreach (SIPDialogueAsset dialogueAsset in dialogueAssets)
+                {
+                    DialogInfo.DialogItems.Add(new SIPEventDialog(dialogueAsset.SIPDialogue.Id.ToString(), "confirmed", dialogueAsset.SIPDialogue));
+                }
+
+                MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.NotifySent, "Full state notification for dialog and " + ResourceURI.ToString() + ".", SubscriptionDialogue.Owner));
+            }
+            catch (Exception excp)
+            {
+                logger.Error("Exception SIPDialogEventSubscription GetFullState. " + excp.Message);
+            }
+        }
+
+        public override string GetNotifyBody()
+        {
+            return DialogInfo.ToXMLText();
+        }
+
+        public override void AddMonitorEvent(SIPMonitorMachineEvent machineEvent)
+        {
+            try
+            {
+                MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.Monitor, "Monitor event " + machineEvent.MachineEventType + " dialog " + ResourceURI.ToString() + " (ID " + machineEvent.ResourceID + ").", SubscriptionDialogue.Owner));
+
+                string state = GetStateForEventType(machineEvent.MachineEventType);
+
+                if (machineEvent.MachineEventType == SIPMonitorMachineEventTypesEnum.SIPDialogueRemoved)
+                {
+                    DialogInfo.DialogItems.Add(new SIPEventDialog(machineEvent.ResourceID, state, null));
+                }
+                else
+                {
+                    SIPDialogueAsset sipDialogue = GetDialogue_External(new Guid(machineEvent.ResourceID));
+
+                    if (sipDialogue == null)
+                    {
+                        // Couldn't find the dialogue in the database so it must be terminated.
+                        DialogInfo.DialogItems.Add(new SIPEventDialog(machineEvent.ResourceID, "terminated", null));
+                    }
+                    else
+                    {
+                        DialogInfo.DialogItems.Add(new SIPEventDialog(sipDialogue.Id.ToString(), state, sipDialogue.SIPDialogue));
+                    }
+                }
+            }
+            catch (Exception excp)
+            {
+                logger.Error("Exception SIPDialogEventSubscription AddMonitorEvent. " + excp.Message);
+                throw;
+            }
+        }
+
+        public override void NotificationSent()
+        {
+            if (DialogInfo.State == SIPEventDialogInfoStateEnum.full)
+            {
+                MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.NotifySent, "Full state notification sent for " + DialogInfo.Entity.ToString() + ", version " + DialogInfo.Version + ", cseq " + SubscriptionDialogue.CSeq + ".", SubscriptionDialogue.Owner));
+            }
+            else
+            {
+                foreach (SIPEventDialog dialog in DialogInfo.DialogItems)
+                {
+                    string remoteURI = (dialog.RemoteParticipant != null && dialog.RemoteParticipant.URI != null) ? ", " + dialog.RemoteParticipant.URI.ToString() : null;
+                    MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.NotifySent, "Partial state notification sent for " + DialogInfo.Entity.ToString() + " dialog ID " + dialog.ID + " " + dialog.State + ", version " + DialogInfo.Version + remoteURI + ", cseq " + SubscriptionDialogue.CSeq + ".", SubscriptionDialogue.Owner));
+                }
+            }
+
+            DialogInfo.State = SIPEventDialogInfoStateEnum.partial;
+            DialogInfo.DialogItems.Clear();
+            DialogInfo.Version++;
+        }
+
+        private string GetStateForEventType(SIPMonitorMachineEventTypesEnum machineEventType)
+        {
+            switch (machineEventType)
+            {
+                case SIPMonitorMachineEventTypesEnum.SIPDialogueCreated: return "confirmed";
+                case SIPMonitorMachineEventTypesEnum.SIPDialogueRemoved: return "terminated";
+                case SIPMonitorMachineEventTypesEnum.SIPDialogueUpdated: return "updated";
+                default: throw new ApplicationException("The state for a dialog SIP event could not be determined from the monitor event type of " + machineEventType + ".");
+            }
+        }
+    }
+}
---
+++ sipsorcery-core/SIPSorcery.SIP.App/SIPNotifications/SIPPresenceEventSubscription.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SIPSorcery.Persistence;
+
+namespace SIPSorcery.SIP.App
+{
+    public class SIPPresenceEventSubscription : SIPEventSubscription
+    {
+        private const int MAX_SIPACCOUNTS_TO_RETRIEVE = 25;
+
+        private static string m_wildcardUser = SIPMonitorFilter.WILDCARD;
+        private static string m_contentType = SIPMIMETypes.PRESENCE_NOTIFY_CONTENT_TYPE;
+
+        private SIPEventPresence Presence;
+
+        private SIPAssetGetListDelegate<SIPAccount> GetSIPAccounts_External;
+        private SIPAssetCountDelegate<SIPRegistrarBinding> GetSIPRegistrarBindingsCount_External;
+
+        public override SIPEventPackage SubscriptionEventPackage
+        {
+            get { return SIPEventPackage.Presence; }
+        }
+
+        public override string MonitorFilter
+        {
+            get { return "presence " + ResourceURI.ToString(); }
+        }
+
+        public override string NotifyContentType
+        {
+            get { return m_contentType; }
+        }
+
+        public SIPPresenceEventSubscription(
+            SIPMonitorLogDelegate log,
+            string sessionID,
+            SIPURI resourceURI,
+            SIPURI canonincalResourceURI,
+            string filter,
+            SIPDialogue subscriptionDialogue,
+            int expiry,
+            SIPAssetGetListDelegate<SIPAccount> getSIPAccounts,
+            SIPAssetCountDelegate<SIPRegistrarBinding> getBindingsCount
+            )
+            : base(log, sessionID, resourceURI, canonincalResourceURI, filter, subscriptionDialogue, expiry)
+        {
+            GetSIPAccounts_External = getSIPAccounts;
+            GetSIPRegistrarBindingsCount_External = getBindingsCount;
+            Presence = new SIPEventPresence(resourceURI);
+        }
+
+        public override void GetFullState()
+        {
+            try
+            {
+                List<SIPAccount> sipAccounts = null;
+
+                if (ResourceURI.User == m_wildcardUser)
+                {
+                    sipAccounts = GetSIPAccounts_External(s => s.Owner == SubscriptionDialogue.Owner, "SIPUsername", 0, MAX_SIPACCOUNTS_TO_RETRIEVE);
+                }
+                else
+                {
+                    sipAccounts = GetSIPAccounts_External(s => s.SIPUsername == CanonicalResourceURI.User && s.SIPDomain == CanonicalResourceURI.Host, "SIPUsername", 0, MAX_SIPACCOUNTS_TO_RETRIEVE);
+                }
+
+                foreach (SIPAccount sipAccount in sipAccounts)
+                {
+                    SIPURI aor = SIPURI.ParseSIPURIRelaxed(sipAccount.SIPUsername + "@" + sipAccount.SIPDomain);
+
+                    int bindingsCount = GetSIPRegistrarBindingsCount_External(b => b.SIPAccountId == sipAccount.Id);
+                    if (bindingsCount > 0)
+                    {
+                        string safeSIPAccountID = sipAccount.Id.ToString();
+                        Presence.Tuples.Add(new SIPEventPresenceTuple(safeSIPAccountID, SIPEventPresenceStateEnum.open, aor, Decimal.Zero));
+                        //logger.Debug(" full presence " + aor.ToString() + " open.");
+                    }
+                    else
+                    {
+                        string safeSIPAccountID = sipAccount.Id.ToString();
+                        Presence.Tuples.Add(new SIPEventPresenceTuple(safeSIPAccountID, SIPEventPresenceStateEnum.closed, null, Decimal.Zero));
+                        //logger.Debug(" full presence " + aor.ToString() + " closed.");
+                    }
+                }
+
+                MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.NotifySent, "Full state notification for presence and " + ResourceURI.ToString() + ".", SubscriptionDialogue.Owner));
+            }
+            catch (Exception excp)
+            {
+                logger.Error("Exception SIPPresenceEventSubscription GetFullState. " + excp.Message);
+            }
+        }
+
+        public override string GetNotifyBody()
+        {
+            return Presence.ToXMLText();
+        }
+
+        public override void AddMonitorEvent(SIPMonitorMachineEvent machineEvent)
+        {
+            try
+            {
+                MonitorLogEvent_External(new SIPMonitorConsoleEvent(SIPMonitorServerTypesEnum.Notifier, SIPMonitorEventTypesEnum.Monitor, "Monitor event " + machineEvent.MachineEventType + " presence " + machineEvent.ResourceURI.ToString() + " for subscription to " + ResourceURI.ToString() + ".", SubscriptionDialogue.Owner));
+
+                string safeSIPAccountID = machineEvent.ResourceID;
+                SIPURI sipAccountURI = machineEvent.ResourceURI;
+
+                if (machineEvent.MachineEventType == SIPMonitorMachineEventTypesEnum.SIPRegistrarBindingUpdate)
+                {
+                    // A binding has been updated so there is at least one device online for the SIP account.
+                    Presence.Tuples.Add(new SIPEventPresenceTuple(safeSIPAccountID, SIPEventPresenceStateEnum.open, sipAccountURI, Decimal.Zero));
+                    //logger.Debug(" single presence open.");
+                }
+                else
+                {
+                    // A binding has been removed but there could still be others.
+                    Guid sipAccountID = new Guid(machineEvent.ResourceID);
+                    int bindingsCount = GetSIPRegistrarBindingsCount_External(b => b.SIPAccountId == sipAccountID);
+                    if (bindingsCount > 0)
+                    {
+                        Presence.Tuples.Add(new SIPEventPresenceTuple(safeSIPAccountID, SIPEventPresenceStateEnum.open, sipAccountURI, Decimal.Zero));
+                    }
+                    else
+                    {
+                        Presence.Tuples.Add(new SIPEventPresenceTuple(safeSIPAccountID, SIPEventPresenceStateEnum.closed, sipAccountURI, Decimal.Zero));
+                    }
+                }
+            }
+            catch (Exception excp)
+            {
+                logger.Error("Exception SIPresenceEventSubscription AddMonitorEvent. " + excp.Message);
+                throw;
+            }
+        }
+
+        public override void NotificationSent()
+        {
+            Presence.Tuples.Clear();
+        }
+    }
+}
