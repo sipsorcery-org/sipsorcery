596e8764762a122e6fa691ab47467582cb42d4ec
rj2 <jaenike@ferrari-electronic.de>
2016-06-20 14:42:34 +0200

    sample application to reproduce the reinvite ack bug need to change the sip address to a working client in line 220 of program.cs to make it work in other environment

---
+++ ack_bug_track/ack_bug_track/Model/CallLeg.cs
@@ -0,0 +1,100 @@
+﻿using SIPSorcery.SIP.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIPSorcery.SIP;
+
+namespace ack_bug_track.Model
+{
+	public class CallLeg : IDisposable
+	{
+		public CallLegType LegType
+		{
+			get;
+			set;
+		}
+		public ISIPClientUserAgent SipClient
+		{
+			get;
+			set;
+		}
+		public ISIPServerUserAgent SipServer
+		{
+			get;
+			set;
+		}
+		public CallState CallState
+		{
+			get;
+			set;
+		}
+		public CallLeg PartnerLeg
+		{
+			get;
+			set;
+		}
+		public string CallId
+		{
+			get;
+			set;
+		}
+		public SIPRequest SIPInvite
+		{
+			get;
+			set;
+		}
+		public SIPFromHeader SIPFrom
+		{
+			get;
+			set;
+		}
+		public SIPToHeader SIPTo
+		{
+			get;
+			set;
+		}
+		public SIPContactHeader Contact
+		{
+			get;
+			set;
+		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if(!disposedValue)
+			{
+				if(disposing)
+				{
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~CallLeg() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+
+
+	}
+}
