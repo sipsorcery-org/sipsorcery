b91e177060141e14435b0a8761ce05ce7bc2418d
Dmitriy Tarasov <tarasov65536@gmail.com>
2019-10-29 23:04:58 +0200

    Revert "this hack does not needed anymore"
    This reverts commit 049849c8fabc63f3a0e13f1a3b7c92fd784a3be8.

--- src/app/SIPUserAgents/SIPRegistrationUserAgent.cs
+++ src/app/SIPUserAgents/SIPRegistrationUserAgent.cs
@@ -202,6 +202,13 @@ namespace SIPSorcery.SIP.App
 
                     m_exit = true;
                     m_waitForRegistrationMRE.Set();
+
+                    if (m_isRegistered)
+                    {
+                        m_attempts = 0;
+                        m_expiry = 0;
+                        ThreadPool.QueueUserWorkItem(delegate { SendInitialRegister(); });
+                    }
                 }
             }
             catch (Exception excp)
