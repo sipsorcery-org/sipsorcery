c2b52a842e777fae2f60251bc64bcc98df2472ec
Aaron Clauson <aaron@sipsorcery.com>
2019-11-02 18:45:19 +0000

    Added missing pfx certificate for integration tests. Increased TCP timeouts for integration tests.

--- test/core/SIP/SIPTransportUnitTest.cs
+++ test/core/SIP/SIPTransportUnitTest.cs
@@ -138,7 +138,7 @@ namespace SIPSorcery.SIP.UnitTests
             Task.Run(() => { RunServer(serverChannel, cancelServer); });
             var clientTask = Task.Run(async () => { await RunClient(clientChannel, new SIPURI(SIPSchemesEnum.sip, serverChannel.SIPChannelEndPoint), testComplete); });
 
-            Task.WhenAny(new Task[] { clientTask, Task.Delay(2000) }).Wait();
+            Task.WhenAny(new Task[] { clientTask, Task.Delay(5000) }).Wait();
 
             if (testComplete.Task.IsCompleted == false)
             {
@@ -180,9 +180,7 @@ namespace SIPSorcery.SIP.UnitTests
                 logger.LogDebug($"Server URI {serverUri.ToString()}.");
 
                 var clientTask = Task.Run(async () => { await RunClient(clientChannel, serverUri, testComplete); });
-
-                //Task.WhenAny(new Task[] { clientTask, serverTask, Task.Delay(10000) }).Wait();
-                Task.WhenAny(new Task[] { clientTask, Task.Delay(2000) }).Wait();
+                Task.WhenAny(new Task[] { clientTask, Task.Delay(5000) }).Wait();
 
                 if (testComplete.Task.IsCompleted == false)
                 {
@@ -262,7 +260,7 @@ namespace SIPSorcery.SIP.UnitTests
             var serverTask = Task.Run(() => { RunServer(serverChannel, cancelServer); });
             var clientTask = Task.Run(async () => { await RunClient(clientChannel, new SIPURI(SIPSchemesEnum.sips, serverChannel.SIPChannelEndPoint), testComplete); });
 
-            Task.WhenAny(new Task[] { serverTask, clientTask, Task.Delay(3000) }).Wait();
+            Task.WhenAny(new Task[] { serverTask, clientTask, Task.Delay(5000) }).Wait();
 
             if (testComplete.Task.IsCompleted == false)
             {
