3615c8a5aa117f3307626f42d61371ce71424477
Salih YILDIRIM <salih@salihy.com>
2019-11-26 22:51:54 +0300

    SIPRequest and SIPResponse inherited from common ancestor.  (#105)
    * SIPRequest and SIPResponse inherited from common ancestor. This is better this way if you need to implement your own dialogue infrastructure.
    
    * SipRequest and SipResponse are inherited from common ancestor.

--- src/core/SIP/SIPRequest.cs
+++ src/core/SIP/SIPRequest.cs
@@ -22,7 +22,7 @@ namespace SIPSorcery.SIP
     /// <summary>
     /// Represents a SIP Request.
     /// </summary>
-    public class SIPRequest
+    public class SIPRequest : ISIPMessage
     {
         private static ILogger logger = Log.Logger;
 
--- src/core/SIP/SIPResponse.cs
+++ src/core/SIP/SIPResponse.cs
@@ -28,7 +28,7 @@ namespace SIPSorcery.SIP
     /// <summary>
     /// Represents a SIP Response.
     /// </summary>
-    public class SIPResponse
+    public class SIPResponse : ISIPMessager
     {
         private static ILogger logger = Log.Logger;
 
