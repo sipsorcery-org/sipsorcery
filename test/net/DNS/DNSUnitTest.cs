a881221232276967ef4b246d1d4b354748379d1b
Ralf Jänike <jaenike@ferrari-electronic.de>
2019-12-16 12:13:09 +0100

    DNSUnitTest: Add Test for Async-Resolution and DNS.QType.ANY -also tests IPSocket.Parse

--- test/net/DNS/DNSUnitTest.cs
+++ test/net/DNS/DNSUnitTest.cs
@@ -17,6 +17,7 @@ using System.Linq;
 using Heijden.DNS;
 using Microsoft.Extensions.Logging;
 using SIPSorcery.SIP;
+using SIPSorcery.Sys;
 using Xunit;
 
 namespace SIPSorcery.Net.UnitTests
@@ -25,6 +26,64 @@ namespace SIPSorcery.Net.UnitTests
     public class DNSUnitTest
     {
         private static ILogger logger = SIPSorcery.Sys.Log.Logger;
+
+        /// <summary>
+        /// Test DNS resolution
+        /// also test IPSocket.Parse
+        /// </summary>
+        [Fact]
+        public void LookupAnyRecordTest()
+        {
+            logger.LogDebug(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            DNSResponse result = DNSManager.Lookup("dns.google", QType.ANY, 100, null, false, false);
+
+            Assert.NotNull(result);
+
+            Assert.NotEmpty(result.RecordsA);
+            var ipv4Addresses = from a in result.RecordsA select a.Address;
+            Assert.NotEmpty(ipv4Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("8.8.8.8").Address, ipv4Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("8.8.4.4").Address, ipv4Addresses);
+
+            Assert.NotEmpty(result.RecordsAAAA);
+            var ipv6Addresses = from a in result.RecordsAAAA select a.Address;
+            Assert.NotEmpty(ipv6Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("2001:4860:4860::8888").Address, ipv6Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("2001:4860:4860::8844").Address, ipv6Addresses);
+        }
+
+        /// <summary>
+        /// Test async DNS resolution
+        /// also test IPSocket.Parse
+        /// </summary>
+        [Fact]
+        public void LookupAnyRecordAsyncTest()
+        {
+            logger.LogDebug(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            DNSManager.ReStart();
+            DNSResponse result = DNSManager.Lookup("dns.google", QType.ANY, 100, null, true, true);
+            Assert.Null(result);
+
+            result = DNSManager.Lookup("dns.google", QType.ANY, 150, null, true, false);
+
+            DNSManager.Stop();
+
+            Assert.NotNull(result);
+
+            Assert.NotEmpty(result.RecordsA);
+            var ipv4Addresses = from a in result.RecordsA select a.Address;
+            Assert.NotEmpty(ipv4Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("8.8.8.8").Address, ipv4Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("8.8.4.4").Address, ipv4Addresses);
+
+            Assert.NotEmpty(result.RecordsAAAA);
+            var ipv6Addresses = from a in result.RecordsAAAA select a.Address;
+            Assert.NotEmpty(ipv6Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("2001:4860:4860::8888").Address, ipv6Addresses);
+            Assert.Contains(IPSocket.ParseSocketString("2001:4860:4860::8844").Address, ipv6Addresses);
+        }
 
         /// <summary>
         /// Test that a known A record is resolved.
@@ -36,8 +95,13 @@ namespace SIPSorcery.Net.UnitTests
 
             DNSResponse result = DNSManager.Lookup("www.sipsorcery.com", QType.A, 10, null, false, false);
 
+            Assert.NotEmpty(result.RecordsA);
             logger.LogDebug($"Lookup result {result.RecordsA[0].Address}.");
 
+            Assert.Equal("67.222.131.148", result.RecordsA[0].Address.ToString());
+
+            result = DNSManager.Lookup("67.222.131.148", QType.A, 10, null, false, false);
+            logger.LogDebug($"Lookup result {result.RecordsA[0].Address}.");
             Assert.Equal("67.222.131.148", result.RecordsA[0].Address.ToString());
         }
 
@@ -56,7 +120,8 @@ namespace SIPSorcery.Net.UnitTests
                 logger.LogDebug($"AAAA Lookup result {aaaaResult.ToString()}.");
             }
 
-            Assert.True(result.RecordsAAAA.Length > 0);
+            Assert.NotEmpty(result.RecordsAAAA);
+            Assert.True(result.RecordsAAAA[0].Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6);
         }
 
         [Fact]
