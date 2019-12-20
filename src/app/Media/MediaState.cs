6bcf11567e2e8f8bd9e83b78f3c439cf27df2769
Yitzchok <adminjew@gmail.com>
2019-12-20 13:22:25 -0500

    Add event to media state

--- src/app/Media/MediaState.cs
+++ src/app/Media/MediaState.cs
@@ -1,8 +1,26 @@
-﻿namespace SIPSorcery.SIP.App.Media
+﻿using System;
+
+namespace SIPSorcery.SIP.App.Media
 {
     public class MediaState
     {
+        private bool _remoteOnHold;
+
         public virtual bool LocalOnHold { get; set; }
-        public virtual bool RemoteOnHold { get; set; }
+
+        public virtual bool RemoteOnHold
+        {
+            get => _remoteOnHold;
+            set
+            {
+                if (_remoteOnHold != value)
+                {
+                    RemoteOnHoldChanged?.Invoke(value);
+                    _remoteOnHold = value;
+                }
+            }
+        }
+
+        public event Action<bool> RemoteOnHoldChanged;
     }
 }
