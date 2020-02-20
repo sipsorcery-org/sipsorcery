77cfdfaf520fa867f5e1cc6b7e826e4da9805e1e
Aaron Clauson <aaron@sipsorcery.com>
2020-02-20 12:07:16 +0000

    Added netcoreapp3.1 target to RtpAVSession.

--- examples/SIPSorcery.RtpAVSession/RtpAVSession.cs
+++ examples/SIPSorcery.RtpAVSession/RtpAVSession.cs
@@ -15,9 +15,7 @@
 //-----------------------------------------------------------------------------
 
 using System;
-using System.Linq;
 using System.Net.Sockets;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NAudio.Wave;
