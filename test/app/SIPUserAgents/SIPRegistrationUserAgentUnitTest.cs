1d60bd07686cdf8d670e41199a8e4118e0ba989d
Rory Roberts <rory.the.mover@gmail.com>
2020-03-27 12:17:53 +0000

    Added support for adjusting invite and register requests
    Resolves #176

--- test/app/SIPUserAgents/SIPRegistrationUserAgentUnitTest.cs
+++ test/app/SIPUserAgents/SIPRegistrationUserAgentUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using Microsoft.Extensions.Logging;
 using SIPSorcery.SIP;
 using SIPSorcery.SIP.App;
@@ -42,6 +43,45 @@ namespace SIPSorcery.UnitTests.app.SIPUserAgents
             
             channel.SIPMessageSent.WaitOne(5000);
             Assert.Contains("My-Header: value", channel.LastSIPMessageSent);
+
+            userAgent.Stop();
+        }
+
+        [Fact]
+        public void RegisterWithAdjustedRegisterHeaderTest()
+        {
+            logger.LogDebug("--> " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            logger.BeginScope(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            SIPTransport transport = new SIPTransport();
+            MockSIPChannel channel = new MockSIPChannel(new System.Net.IPEndPoint(IPAddress.Any, 0));
+            transport.AddSIPChannel(channel);
+            SIPRegistrationUserAgent userAgent = new SIPRegistrationUserAgent(
+                transport,
+                null,
+                new SIPURI("alice", "192.168.11.50", null, SIPSchemesEnum.sip, SIPProtocolsEnum.udp),
+                "alice",
+                "password123",
+                null,
+                "192.168.11.50",
+                new SIPURI(SIPSchemesEnum.sip, IPAddress.Any, 0),
+                120,
+                null,
+                new[] { "My-Header: value" });
+            SIPContactHeader testHeader = new SIPContactHeader("Contact Name", new SIPURI("User", "Host", "Param=Value"));
+            userAgent.AdjustRegister = register =>
+            {
+                register.Header.Contact = new List<SIPContactHeader>
+                {
+                    testHeader
+                };
+                return register;
+            };
+
+            userAgent.Start();
+
+            channel.SIPMessageSent.WaitOne(5000);
+            Assert.Contains(testHeader.ToString(), channel.LastSIPMessageSent);
 
             userAgent.Stop();
         }
