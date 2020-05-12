bebd77c02a372ca993f1f98e9a332fffec6167b8
Aaron Clauson <aaron@sipsorcery.com>
2020-05-12 13:09:00 +0100

    Added new example to test WebRTC with no signalling required.

--- src/net/WebRTC/IRTCPeerConnection.cs
+++ src/net/WebRTC/IRTCPeerConnection.cs
@@ -202,6 +202,11 @@ namespace SIPSorcery.Net
 
     /// <summary>
     /// The states a peer connection transitions through.
+    /// The difference between the IceConnectionState and the PeerConnectionState is somewhat subtle:
+    /// - IceConnectionState: applies to the connection checks amongst ICE candidates and is
+    ///   set as completed as soon as a local and remote candidate have set their nominated candidate,
+    /// - PeerConnectionState: takes into account the IceConnectionState but also includes the DTLS
+    ///   handshake and actions at the application layer such as a request to close the peer connection.
     /// </summary>
     /// <remarks>
     /// As specified in https://www.w3.org/TR/webrtc/#rtcpeerconnectionstate-enum.
