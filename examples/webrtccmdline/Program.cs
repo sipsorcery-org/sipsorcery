c625e0381e192117a6757425130ead6a2451c3cc
Aaron Clauson <aaron@sipsorcery.com>
2020-07-13 15:31:21 +0100

    Added command line options to webrtccmdline app.

--- examples/webrtccmdline/Program.cs
+++ examples/webrtccmdline/Program.cs
@@ -9,6 +9,7 @@
 // 
 // History:
 // 28 Mar 2020	Aaron Clauson	Created, Dublin, Ireland.
+// 13 Jul 2020  Aaron CLauson   Added command line options.
 //
 // License: 
 // BSD 3-Clause "New" or "Revised" License, see included LICENSE.md file.
@@ -19,9 +20,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using CommandLine;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Serilog;
 using SIPSorcery.Net;
 using SIPSorcery.Sys;
@@ -31,6 +35,25 @@ using WebSocketSharp.Server;
 
 namespace SIPSorcery.Examples
 {
+    public class Options
+    {
+        [Option("ws", Required = false,
+            HelpText = "Create a web socket server to act as a signalling channel for exchanging SDP and ICE candidates with a remote WebRTC peer.")]
+        public bool UseWebSocket { get; set; }
+
+        [Option("wss", Required = false,
+            HelpText = "Create a secure web socket server to act as a signalling channel for exchanging SDP and ICE candidates with a remote WebRTC peer.")]
+        public bool UseSecureWebSocket { get; set; }
+
+        [Option("offer", Required = false,
+            HelpText = "Create an initial SDP offer for a remote WebRTC peer. The offer will be serialised as base 64 encoded JSON. An SDP answer in the same format can then be entered.")]
+        public bool CreateJsonOffer { get; set; }
+
+        [Option("stun", Required = false,
+            HelpText = "STUN or TURN server to use in the peer connection configuration. Format \"(stun|turn):host[:port][;username;password]\".")]
+        public string StunServer { get; set; }
+    }
+
     public class WebRtcClient : WebSocketBehavior
     {
         public RTCPeerConnection pc;
@@ -57,44 +80,115 @@ namespace SIPSorcery.Examples
     {
         private const string LOCALHOST_CERTIFICATE_PATH = "certs/localhost.pfx";
         private const int WEBSOCKET_PORT = 8081;
-        private const string SIPSORCERY_STUN_SERVER = "turn:sipsorcery.com";
-        private const string SIPSORCERY_STUN_SERVER_USERNAME = "aaron"; //"stun.sipsorcery.com";
-        private const string SIPSORCERY_STUN_SERVER_PASSWORD = "password"; //"stun.sipsorcery.com";
+        //private const string SIPSORCERY_STUN_SERVER = "turn:sipsorcery.com";
+        //private const string SIPSORCERY_STUN_SERVER_USERNAME = "aaron"; //"stun.sipsorcery.com";
+        //private const string SIPSORCERY_STUN_SERVER_PASSWORD = "password"; //"stun.sipsorcery.com";
 
         private static Microsoft.Extensions.Logging.ILogger logger = SIPSorcery.Sys.Log.Logger;
 
         private static WebSocketServer _webSocketServer;
+        private static RTCIceServer _stunServer;
 
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("ICE Console Test Program");
+            Console.WriteLine("WebRTC Console Test Program");
             Console.WriteLine("Press ctrl-c to exit.");
 
+            var result = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => RunCommand(opts).Wait());
+        }
+
+        static async Task RunCommand(Options options)
+        {
             // Plumbing code to facilitate a graceful exit.
             CancellationTokenSource exitCts = new CancellationTokenSource(); // Cancellation token to stop the SIP transport and RTP stream.
             ManualResetEvent exitMre = new ManualResetEvent(false);
 
             AddConsoleLogger();
 
-            // Start web socket.
-            Console.WriteLine("Starting web socket server...");
-            _webSocketServer = new WebSocketServer(IPAddress.Any, WEBSOCKET_PORT, true);
-            _webSocketServer.SslConfiguration.ServerCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(LOCALHOST_CERTIFICATE_PATH);
-            _webSocketServer.SslConfiguration.CheckCertificateRevocation = false;
-            //_webSocketServer.Log.Level = WebSocketSharp.LogLevel.Debug;
-            _webSocketServer.AddWebSocketService<WebRtcClient>("/sendoffer", (client) =>
+            if (options.UseWebSocket || options.UseSecureWebSocket)
             {
-                client.WebSocketOpened += SendOffer;
-                client.OnMessageReceived += WebSocketMessageReceived;
-            });
-            _webSocketServer.AddWebSocketService<WebRtcClient>("/receiveoffer", (client) =>
-            {
-                client.WebSocketOpened += ReceiveOffer;
-                client.OnMessageReceived += WebSocketMessageReceived;
-            });
-            _webSocketServer.Start();
+                // Start web socket.
+                Console.WriteLine("Starting web socket server...");
+                _webSocketServer = new WebSocketServer(IPAddress.Any, WEBSOCKET_PORT, options.UseSecureWebSocket);
 
-            Console.WriteLine($"Waiting for browser web socket connection to {_webSocketServer.Address}:{_webSocketServer.Port}...");
+                if (options.UseSecureWebSocket)
+                {
+                    _webSocketServer.SslConfiguration.ServerCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(LOCALHOST_CERTIFICATE_PATH);
+                    _webSocketServer.SslConfiguration.CheckCertificateRevocation = false;
+                }
+
+                //_webSocketServer.Log.Level = WebSocketSharp.LogLevel.Debug;
+                _webSocketServer.AddWebSocketService<WebRtcClient>("/sendoffer", (client) =>
+                {
+                    client.WebSocketOpened += SendOffer;
+                    client.OnMessageReceived += WebSocketMessageReceived;
+                });
+                _webSocketServer.AddWebSocketService<WebRtcClient>("/receiveoffer", (client) =>
+                {
+                    client.WebSocketOpened += ReceiveOffer;
+                    client.OnMessageReceived += WebSocketMessageReceived;
+                });
+                _webSocketServer.Start();
+
+                Console.WriteLine($"Waiting for browser web socket connection to {_webSocketServer.Address}:{_webSocketServer.Port}...");
+            }
+
+            if (options.StunServer != null)
+            {
+                string[] fields = options.StunServer.Split(';');
+
+                _stunServer = new RTCIceServer
+                {
+                    urls = fields[0],
+                    username = fields.Length > 1 ? fields[1] : null,
+                    credential = fields.Length > 2 ? fields[2] : null,
+                    credentialType = RTCIceCredentialType.password
+                };
+            }
+
+            if (options.CreateJsonOffer)
+            {
+                var pc = Createpc(null, _stunServer);
+
+                var offerSdp = pc.createOffer(null);
+                await pc.setLocalDescription(offerSdp);
+
+                Console.WriteLine(offerSdp.sdp);
+
+                var offerJson = JsonConvert.SerializeObject(offerSdp, new Newtonsoft.Json.Converters.StringEnumConverter());
+                var offerBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(offerJson));
+
+                Console.WriteLine(offerBase64);
+
+                string remoteAnswerB64 = null;
+                while (string.IsNullOrWhiteSpace(remoteAnswerB64))
+                {
+                    Console.Write("Remote Answer => ");
+                    remoteAnswerB64 = Console.ReadLine();
+                }
+
+                if (remoteAnswerB64 == "q")
+                {
+                    Console.WriteLine("Quitting.");
+                }
+                else
+                {
+                    string remoteAnswer = Encoding.UTF8.GetString(Convert.FromBase64String(remoteAnswerB64));
+                    //Console.WriteLine($"Remote answer: {remoteAnswer}");
+
+                    RTCSessionDescriptionInit answerInit = JsonConvert.DeserializeObject<RTCSessionDescriptionInit>(remoteAnswer);
+                    pc.setRemoteDescription(answerInit);
+
+                    // Wait for a signal saying the call failed, was cancelled with ctrl-c or completed.
+                    exitMre.WaitOne();
+
+                    Console.WriteLine("Closing.");
+                    pc.Close("normal");
+
+                    Task.Delay(1000).Wait();
+                }
+            }
 
             // Ctrl-c will gracefully exit the call at any point.
             Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
@@ -106,13 +200,13 @@ namespace SIPSorcery.Examples
             // Wait for a signal saying the call failed, was cancelled with ctrl-c or completed.
             exitMre.WaitOne();
 
-            _webSocketServer.Stop();
+            _webSocketServer?.Stop();
         }
 
         private static Task<RTCPeerConnection> ReceiveOffer(WebSocketContext context)
         {
             logger.LogDebug($"Web socket client connection from {context.UserEndPoint}, waiting for offer...");
-            var pc = Createpc(context);
+            var pc = Createpc(context, _stunServer);
             return Task.FromResult(pc);
         }
 
@@ -120,7 +214,7 @@ namespace SIPSorcery.Examples
         {
             logger.LogDebug($"Web socket client connection from {context.UserEndPoint}, sending offer.");
 
-            var pc = Createpc(context);
+            var pc = Createpc(context, _stunServer);
 
             var offerInit = pc.createOffer(null);
             await pc.setLocalDescription(offerInit);
@@ -132,7 +226,7 @@ namespace SIPSorcery.Examples
             return pc;
         }
 
-        private static RTCPeerConnection Createpc(WebSocketContext context)
+        private static RTCPeerConnection Createpc(WebSocketContext context, RTCIceServer stunServer)
         {
             List<RTCCertificate> presetCertificates = null;
             if (File.Exists(LOCALHOST_CERTIFICATE_PATH))
@@ -144,16 +238,8 @@ namespace SIPSorcery.Examples
             RTCConfiguration pcConfiguration = new RTCConfiguration
             {
                 certificates = presetCertificates,
-                X_RemoteSignallingAddress = context.UserEndPoint.Address,
-                iceServers = new List<RTCIceServer> {
-                    new RTCIceServer
-                    {
-                        urls = SIPSORCERY_STUN_SERVER,
-                        username = SIPSORCERY_STUN_SERVER_USERNAME,
-                        credential = SIPSORCERY_STUN_SERVER_PASSWORD,
-                        credentialType = RTCIceCredentialType.password
-                    }
-                },
+                X_RemoteSignallingAddress = (context != null) ? context.UserEndPoint.Address : null,
+                iceServers = stunServer != null ? new List<RTCIceServer> { stunServer } : null,
                 iceTransportPolicy = RTCIceTransportPolicy.all
             };
 
@@ -175,7 +261,10 @@ namespace SIPSorcery.Examples
                 if (pc.signalingState == RTCSignalingState.have_local_offer ||
                     pc.signalingState == RTCSignalingState.have_remote_offer)
                 {
-                    context.WebSocket.Send($"candidate:{candidate}");
+                    if (context != null)
+                    {
+                        context.WebSocket.Send($"candidate:{candidate}");
+                    }
                 }
             };
 
