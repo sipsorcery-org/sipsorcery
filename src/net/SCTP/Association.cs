0a38344ed76fae81f5470aa031cc1d9bd8b9965d
Mark Reed <mark@syxsense.com>
2020-08-05 08:46:36 -0600

    attempt to make the datachannel more reliable for large messages

--- src/net/SCTP/Association.cs
+++ src/net/SCTP/Association.cs
@@ -17,10 +17,12 @@
 // Modified by Andrés Leone Gámez
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Crypto.Tls;
 using Org.BouncyCastle.Security;
@@ -276,29 +278,47 @@ namespace SIPSorcery.Net.Sctp
             }
         }
 
+        private BlockingCollection<ByteBuffer> queue = new BlockingCollection<ByteBuffer>();
+        private ConcurrentQueue<byte[]> bufferQueue = new ConcurrentQueue<byte[]>();
+
+        private void ProcessQueue(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                while (queue.TryTake(out var pbb, 1000))
+                {
+                    Packet rec = new Packet(pbb);
+                    deal(rec);
+                    bufferQueue.Enqueue(pbb.Data);
+                }
+            }
+        }
+
         void startRcv()
         {
             Association me = this;
+            var cts = new CancellationTokenSource();
             _rcv = new Thread(() =>
             {
                 try
                 {
-                    byte[] buf = new byte[_transp.GetReceiveLimit()];
                     while (_rcv != null)
                     {
                         try
                         {
+                            if (!bufferQueue.TryDequeue(out var buf))
+                            {
+                                buf = new byte[_transp.GetReceiveLimit()];
+                            }
                             int length = _transp.Receive(buf, 0, buf.Length, TICK);
                             if (length == DtlsSrtpTransport.DTLS_RECEIVE_ERROR_CODE)
                             {
                                 // The DTLS transport has been closed or i no longer available.
                                 break;
                             }
-                            //logger.LogDebug("SCTP message received: " + Packet.getHex(buf, 0, length));
                             ByteBuffer pbb = new ByteBuffer(buf);
                             pbb.Limit = length;
-                            Packet rec = new Packet(pbb);
-                            deal(rec);
+                            queue.Add(pbb);
                         }
                         catch (SocketException e)
                         {
@@ -326,10 +346,16 @@ namespace SIPSorcery.Net.Sctp
                 {
                     logger.LogDebug("Association receive failed " + ex.GetType().Name + " " + ex.ToString());
                 }
+                finally
+                {
+                    cts.Cancel();
+                }
             });
             _rcv.Priority = ThreadPriority.AboveNormal;
             _rcv.Name = "AssocRcv" + __assocNo;
             _rcv.Start();
+
+            Task.Run(() => ProcessQueue(cts.Token));
         }
 
         /**
@@ -1015,6 +1041,10 @@ namespace SIPSorcery.Net.Sctp
 
         public SCTPStream delStream(int s)
         {
+            if (!_streams.ContainsKey(s))
+            {
+                return null;
+            }
             var st = _streams[s];
             _streams.Remove(s);
             return st;
