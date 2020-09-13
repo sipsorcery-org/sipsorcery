dc4e035febe7bca542e3c997f43ec9223e43f440
Aaron Clauson <aaron@sipsorcery.com>
2020-09-13 19:09:34 +0100

    Removed redundant using statements.

--- examples/WebRTCExamples/WebRTCWebcam/Program.cs
+++ examples/WebRTCExamples/WebRTCWebcam/Program.cs
@@ -19,16 +19,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Serilog;
 using Serilog.Extensions.Logging;
 using SIPSorcery.Media;
 using SIPSorcery.Net;
-using SIPSorceryMedia.Abstractions.V1;
 using SIPSorceryMedia.Windows;
-using WebSocketSharp.Net.WebSockets;
 using WebSocketSharp.Server;
 
 namespace demo
