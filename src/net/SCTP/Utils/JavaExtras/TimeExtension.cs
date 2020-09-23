d042d917644c14d8b15ce9c2be5d7d27de3a2dbd
Aaron Clauson <aaron@sipsorcery.com>
2020-09-23 22:30:44 +0100

    Added missing TimeExtension class file.

---
+++ src/net/SCTP/Utils/JavaExtras/TimeExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class TimeExtension
+{
+    private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    public static long CurrentTimeMillis()
+    {
+        return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+    }
+}
