5ae1488f529bb897a4541782beb0f999ce5f7137
Aaron Clauson <aaron@sipsorcery.com>
2020-10-29 12:47:37 +0000

    Added SIP user agent call.

--- examples/SIPExamples/iOSSIPGetStarted/ViewController.cs
+++ examples/SIPExamples/iOSSIPGetStarted/ViewController.cs
@@ -1,13 +1,20 @@
 ﻿using Foundation;
 using System;
+using System.Threading.Tasks;
 using UIKit;
+using SIPSorcery.Media;
+using SIPSorcery.SIP.App;
 
 namespace iOSSIPGetStarted
 {
     public partial class ViewController : UIViewController
     {
+        SIPUserAgent _userAgent;
+
         public ViewController (IntPtr handle) : base (handle)
         {
+            _userAgent = new SIPUserAgent();
+            CancelButton.Enabled = false;
         }
 
         public override void ViewDidLoad ()
@@ -28,7 +35,21 @@ namespace iOSSIPGetStarted
         /// <param name="sender"></param>
         partial void UIButton201_TouchUpInside(UIButton sender)
         {
-            
+            // DestinationTextBox.Text 
+            CallButton.Enabled = false;
+            CancelButton.Enabled = true;
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var callResult = await _userAgent.Call(DestinationTextBox.Text, null, null, new AudioSendOnlyMediaSession());
+                }
+                catch(Exception excp)
+                {
+                    Console.WriteLine($"Exception {excp}");
+                }
+            });
         }
 
         /// <summary>
@@ -37,7 +58,8 @@ namespace iOSSIPGetStarted
         /// <param name="sender"></param>
         partial void UIButton202_TouchUpInside(UIButton sender)
         {
-            
+            CancelButton.Enabled = false;
+            CallButton.Enabled = true;
         }
     }
 }
