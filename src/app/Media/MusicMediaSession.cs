bbc7c0dac910eae219bafbdb70df97f523cd58c0
Martin Murphy <martin.murphy@whiteboard-it.com>
2020-11-27 13:25:51 -0600

    Add MusicMediaSession which enables RTPSession capable of sending music without mic or speaker integration or bidirectional handshake

---
+++ src/app/Media/MusicMediaSession.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using SIPSorcery.Media;
+using SIPSorcery.Net;
+using SIPSorceryMedia.Abstractions.V1;
+
+namespace SIPSorcery.Media
+{
+    public class MusicMediaSession : VoIPMediaSession
+    {
+        private readonly MediaStreamTrack _mediaStream;
+
+        public MusicMediaSession(AudioFormat sourceAudioFormat, IPEndPoint destination) : base(new MediaEndPoints { AudioSource = new EmptyAudioSource() })
+        {
+            SetDestination(SDPMediaTypesEnum.audio, destination, null);
+            AudioExtrasSource.SetAudioSourceFormat(sourceAudioFormat);
+            AudioExtrasSource.SetSource(AudioSourcesEnum.Music);
+            _mediaStream = new MediaStreamTrack(SDPMediaTypesEnum.audio, true, AudioLocalTrack.Capabilities);
+        }
+
+        public async Task SendAudioFile(string fileToPlay)
+        {
+            await AudioExtrasSource.SendAudioFromStream(
+                        new FileStream(fileToPlay, FileMode.Open, FileAccess.Read, FileShare.Read), AudioSamplingRatesEnum.Rate8KHz);
+
+        }
+        public override MediaStreamTrack AudioRemoteTrack
+        {
+            get
+            {
+                return _mediaStream;
+            }
+        }
+    }
+}
