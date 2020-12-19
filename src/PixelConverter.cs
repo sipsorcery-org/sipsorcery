a786969f05f8d086fb47f1ef94f6f31d1a99d3d5
Aaron Clauson <aaron@sipsorcery.com>
2020-12-19 15:34:10 +0000

    Updated pixel conversion functions to deal with image buffers of uneven dimensions.

--- src/PixelConverter.cs
+++ src/PixelConverter.cs
@@ -5,58 +5,80 @@ namespace SIPSorceryMedia.Abstractions
 {
     public class PixelConverter
     {
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
         public static byte[] ToI420(int width, int height, byte[] sample, VideoPixelFormatsEnum pixelFormat)
         {
-            byte[] i420Buffer = null;
+            switch (pixelFormat)
+            {
+                case VideoPixelFormatsEnum.I420:
+                    return sample;
+                case VideoPixelFormatsEnum.Bgra:
+                    return PixelConverter.RGBAtoI420(sample, width, height, width * 4);
+                case VideoPixelFormatsEnum.Bgr:
+                    return PixelConverter.BGRtoI420(sample, width, height, width * 3);
+                case VideoPixelFormatsEnum.Rgb:
+                    return PixelConverter.RGBtoI420(sample, width, height, width * 3);
+                default:
+                    throw new ApplicationException($"Pixel format {pixelFormat} does not have an I420 conversion implemented.");
+            }
+        }
 
+        public static byte[] ToI420(int width, int height, int stride, byte[] sample, VideoPixelFormatsEnum pixelFormat)
+        {
             switch (pixelFormat)
             {
                 case VideoPixelFormatsEnum.I420:
                     // No conversion needed.
-                    i420Buffer = sample;
-                    break;
+                    return sample;
                 case VideoPixelFormatsEnum.Bgra:
-                    i420Buffer = PixelConverter.RGBAtoI420(sample, width, height);
-                    break;
+                    return PixelConverter.RGBAtoI420(sample, width, height, stride);
                 case VideoPixelFormatsEnum.Bgr:
-                    i420Buffer = PixelConverter.BGRtoI420(sample, width, height);
-                    break;
+                    return PixelConverter.BGRtoI420(sample, width, height, stride);
                 case VideoPixelFormatsEnum.Rgb:
-                    i420Buffer = PixelConverter.RGBtoI420(sample, width, height);
-                    break;
+                    return PixelConverter.RGBtoI420(sample, width, height, stride);
                 default:
                     throw new ApplicationException($"Pixel format {pixelFormat} does not have an I420 conversion implemented.");
             }
+        }
 
-            return i420Buffer;
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
+        public static byte[] RGBAtoI420(byte[] rgba, int width, int height)
+        {
+            return RGBAtoI420(rgba, width, height, width * 4);
         }
 
         // https://docs.microsoft.com/en-us/previous-versions/visualstudio/hh394035(v=vs.105)
         // http://qiita.com/gomachan7/items/54d43693f943a0986e95
-        public static byte[] RGBAtoI420(byte[] rgba, int width, int height)
+        public static byte[] RGBAtoI420(byte[] rgba, int width, int height, int stride)
         {
-            int size = width * height;
-            int uOffset = size;
-            int vOffset = size + size / 4;
-            int r, g, b, y, u, v;
-            int posn = 0;
+            if (rgba == null || rgba.Length < (width * height))
+            {
+                throw new ApplicationException($"RGBA buffer supplied to RGBAtoI420 was too small, expected {stride * height} but got {rgba?.Length}.");
+            }
 
-            byte[] buffer = new byte[width * height * 3 / 2];
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            int uOffset = ySize;
+            int vOffset = ySize + uvSize / 2;
+            int r, g, b, y, u, v;
+            //int posn = 0;
+
+            byte[] buffer = new byte[ySize + uvSize];
 
             for (int row = 0; row < height; row++)
             {
                 for (int col = 0; col < width; col++)
                 {
-                    r = rgba[posn++] & 0xff;
-                    g = rgba[posn++] & 0xff;
-                    b = rgba[posn++] & 0xff;
-                    posn++; // Skip transparency byte.
+                    r = rgba[row * stride + col * 4] & 0xff;
+                    g = rgba[row * stride + col * 4 + 1] & 0xff;
+                    b = rgba[row * stride + col * 4 + 2] & 0xff;
+                    //posn++; // Skip transparency byte.
 
                     y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
                     u = (int)(-0.147 * r - 0.289 * g + 0.436 * b) + 128;
                     v = (int)(0.615 * r - 0.515 * g - 0.100 * b) + 128;
 
-                    buffer[col + row * width] = (byte)(y > 255 ? 255 : y < 0 ? 0 : y);
+                    buffer[col + row * stride] = (byte)(y > 255 ? 255 : y < 0 ? 0 : y);
 
                     int uvposn = col / 2 + row / 2 * width / 2;
 
@@ -68,57 +90,35 @@ namespace SIPSorceryMedia.Abstractions
             return buffer;
         }
 
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
         public static byte[] RGBtoI420(byte[] rgb, int width, int height)
         {
-            int size = width * height;
-            int uOffset = size;
-            int vOffset = size + size / 4;
-            int r, g, b, y, u, v;
-            int posn = 0;
-
-            byte[] buffer = new byte[width * height * 3 / 2];
-
-            for (int row = 0; row < height; row++)
-            {
-                for (int col = 0; col < width; col++)
-                {
-                    r = rgb[posn++] & 0xff;
-                    g = rgb[posn++] & 0xff;
-                    b = rgb[posn++] & 0xff;
-
-                    y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
-                    u = (int)(-0.147 * r - 0.289 * g + 0.436 * b) + 128;
-                    v = (int)(0.615 * r - 0.515 * g - 0.100 * b) + 128;
-
-                   buffer[col + row * width] = (byte)(y > 255 ? 255 : y < 0 ? 0 : y);
-
-                    int uvposn = col / 2 + row / 2 * width / 2;
-
-                    buffer[uOffset + uvposn] = (byte)(u > 255 ? 255 : u < 0 ? 0 : u);
-                    buffer[vOffset + uvposn] = (byte)(v > 255 ? 255 : v < 0 ? 0 : v);
-                }
-            }
-
-            return buffer;
+            return RGBtoI420(rgb, width, height, width * 3);
         }
 
-        public static byte[] BGRtoI420(byte[] bgr, int width, int height)
+        public static byte[] RGBtoI420(byte[] rgb, int width, int height, int stride)
         {
-            int size = width * height;
-            int uOffset = size;
-            int vOffset = size + size / 4;
-            int r, g, b, y, u, v;
-            int posn = 0;
+            if (rgb == null || rgb.Length < (stride * height))
+            {
+                throw new ApplicationException($"RGB buffer supplied to RGBtoI420 was too small, expected {stride * height} but got {rgb?.Length}.");
+            }
 
-            byte[] buffer = new byte[width * height * 3 / 2];
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            int uOffset = ySize;
+            int vOffset = ySize + uvSize / 2;
+            int r, g, b, y, u, v;
+            //int posn = 0;
+
+            byte[] buffer = new byte[ySize + uvSize];
 
             for (int row = 0; row < height; row++)
             {
                 for (int col = 0; col < width; col++)
                 {
-                    b = bgr[posn++] & 0xff;
-                    g = bgr[posn++] & 0xff;
-                    r = bgr[posn++] & 0xff;
+                    r = rgb[row * stride + col * 3] & 0xff;
+                    g = rgb[row * stride + col * 3 + 1] & 0xff;
+                    b = rgb[row * stride + col * 3 + 2] & 0xff;
 
                     y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
                     u = (int)(-0.147 * r - 0.289 * g + 0.436 * b) + 128;
@@ -136,13 +136,72 @@ namespace SIPSorceryMedia.Abstractions
             return buffer;
         }
 
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
+        public static byte[] BGRtoI420(byte[] bgr, int width, int height)
+        {
+            return BGRtoI420(bgr, width, height, width * 3);
+        }
+
+        public static byte[] BGRtoI420(byte[] bgr, int width, int height, int stride)
+        {
+            if (bgr == null || bgr.Length < (stride * height))
+            {
+                throw new ApplicationException($"BGR buffer supplied to BGRtoI420 was too small, expected {stride * height} but got {bgr?.Length}.");
+            }
+
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            int uOffset = ySize;
+            int vOffset = ySize + uvSize / 2;
+            int r, g, b, y, u, v;
+            //int posn = 0;
+
+            byte[] buffer = new byte[ySize + uvSize];
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    b = bgr[row * stride + col * 3] & 0xff;
+                    g = bgr[row * stride + col * 3 + 1] & 0xff;
+                    r = bgr[row * stride + col * 3 + 2] & 0xff;
+
+                    y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
+                    u = (int)(-0.147 * r - 0.289 * g + 0.436 * b) + 128;
+                    v = (int)(0.615 * r - 0.515 * g - 0.100 * b) + 128;
+
+                    buffer[col + row * width] = (byte)(y > 255 ? 255 : y < 0 ? 0 : y);
+
+                    int uvposn = col / 2 + row / 2 * width / 2;
+
+                    buffer[uOffset + uvposn] = (byte)(u > 255 ? 255 : u < 0 ? 0 : u);
+                    buffer[vOffset + uvposn] = (byte)(v > 255 ? 255 : v < 0 ? 0 : v);
+                }
+            }
+
+            return buffer;
+        }
+
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
         public static byte[] I420toRGB(byte[] data, int width, int height)
         {
-            int size = width * height;
-            int uOffset = size;
-            int vOffset = size + size / 4;
-            byte[] rgb = new byte[size * 3];
-            int posn = 0;
+            return I420toRGB(data, width, height, out _);
+        }
+
+        public static byte[] I420toRGB(byte[] data, int width, int height, out int stride)
+        {
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            if (data == null || data.Length < (ySize + uvSize))
+            {
+                throw new ApplicationException($"I420 buffer supplied to I420toRGB was too small, expected {ySize + uvSize} but got {data?.Length}.");
+            }
+
+            int uOffset = ySize;
+            int vOffset = ySize + ySize / 4;
+            stride = (width * 3 + 3) / 4 * 4;
+            byte[] rgb = new byte[height * stride];
+            //int posn = 0;
             int u, v, y;
             int r, g, b;
 
@@ -160,22 +219,35 @@ namespace SIPSorceryMedia.Abstractions
                     g = (int)(y - 0.395 * u - 0.581 * v);
                     b = (int)(y + 2.302 * u);
 
-                    rgb[posn++] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
-                    rgb[posn++] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
-                    rgb[posn++] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
+                    rgb[row * stride + col * 3] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
+                    rgb[row * stride + col * 3 + 1] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
+                    rgb[row * stride + col * 3 + 2] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
                 }
             }
 
             return rgb;
         }
 
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
         public static byte[] I420toBGR(byte[] data, int width, int height)
         {
-            int size = width * height;
-            int uOffset = size;
-            int vOffset = size + size / 4;
-            byte[] bgr = new byte[size * 3];
-            int posn = 0;
+            return I420toBGR(data, width, height, out _);
+        }
+
+        public static byte[] I420toBGR(byte[] data, int width, int height, out int stride)
+        {
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            if (data == null || data.Length < (ySize + uvSize))
+            {
+                throw new ApplicationException($"I420 buffer supplied to I420toBGR was too small, expected {ySize + uvSize} but got {data?.Length}.");
+            }
+
+            int uOffset = ySize;
+            int vOffset = ySize + uvSize / 2;
+            stride = (width * 3 + 3) / 4 * 4;
+            byte[] bgr = new byte[height * stride];
+            //int posn = 0;
             int u, v, y;
             int r, g, b;
 
@@ -193,21 +265,33 @@ namespace SIPSorceryMedia.Abstractions
                     g = (int)(y - 0.395 * u - 0.581 * v);
                     r = (int)(y + 2.302 * u);
 
-                    bgr[posn++] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
-                    bgr[posn++] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
-                    bgr[posn++] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
+                    bgr[row * stride + col * 3] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
+                    bgr[row * stride + col * 3 + 1] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
+                    bgr[row * stride + col * 3 + 2] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
                 }
             }
 
             return bgr;
         }
 
+        [Obsolete("Use overload with stride parameter in order to deal with uneven dimensions.")]
         public static byte[] NV12toBGR(byte[] data, int width, int height)
         {
-            int size = width * height;
-            int uvOffset = size;
-            byte[] bgr = new byte[size * 3];
-            int posn = 0;
+            return NV12toBGR(data, width, height, width * 3);
+        }
+
+        public static byte[] NV12toBGR(byte[] data, int width, int height, int stride)
+        {
+            int ySize = width * height;
+            int uvSize = ((width + 1) / 2) * ((height + 1) / 2) * 2;
+            if (data == null || data.Length < (ySize + uvSize))
+            {
+                throw new ApplicationException($"NV12 buffer supplied to NV12toBGR was too small, expected {ySize + uvSize} but got {data?.Length}.");
+            }
+
+            int uvOffset = ySize;
+            byte[] bgr = new byte[height * stride];
+            //int posn = 0;
             int u, v, y;
             int r, g, b;
 
@@ -225,9 +309,9 @@ namespace SIPSorceryMedia.Abstractions
                     g = (int)(y - 0.395 * u - 0.581 * v);
                     b = (int)(y + 2.302 * u);
 
-                    bgr[posn++] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
-                    bgr[posn++] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
-                    bgr[posn++] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
+                    bgr[row * stride + col * 3] = (byte)(b > 255 ? 255 : b < 0 ? 0 : b);
+                    bgr[row * stride + col * 3 + 1] = (byte)(g > 255 ? 255 : g < 0 ? 0 : g);
+                    bgr[row * stride + col * 3 + 2] = (byte)(r > 255 ? 255 : r < 0 ? 0 : r);
                 }
             }
 
--- test/SIPSorceryMedia.Abstractions.UnitTest/PixelConverterTest.cs
+++ test/SIPSorceryMedia.Abstractions.UnitTest/PixelConverterTest.cs
@@ -43,14 +43,14 @@ namespace SIPSorceryMedia.Abstractions.UnitTest
             // BGR formats get recognised as RGB but when rendered by WPF are treated as BGR.
             Assert.Equal(PixelFormat.Format24bppRgb, bmp.PixelFormat);
 
-            byte[] buffer = BitmapToBuffer(bmp);
+            byte[] buffer = BitmapToBuffer(bmp, out int stride);
 
-            byte[] i420 = PixelConverter.BGRtoI420(buffer, bmp.Width, bmp.Height);
-            byte[] bgr = PixelConverter.I420toBGR(i420, bmp.Width, bmp.Height);
+            byte[] i420 = PixelConverter.BGRtoI420(buffer, bmp.Width, bmp.Height, stride);
+            byte[] bgr = PixelConverter.I420toBGR(i420, bmp.Width, bmp.Height, out int rtStride);
 
             fixed (byte* s = bgr)
             {
-                Bitmap roundTripBmp = new Bitmap(bmp.Width, bmp.Height, (int)bmp.Width * 3, PixelFormat.Format24bppRgb, (IntPtr)s);
+                Bitmap roundTripBmp = new Bitmap(bmp.Width, bmp.Height, rtStride, PixelFormat.Format24bppRgb, (IntPtr)s);
                 roundTripBmp.Save("RoundtripBgr24ToI420Test.bmp");
                 roundTripBmp.Dispose();
             }
@@ -62,17 +62,17 @@ namespace SIPSorceryMedia.Abstractions.UnitTest
         /// Tests that a known image in I420 (ffmpeg -pix_fmt yuv420p) can be converted to a bitmap.
         /// </summary>
         [Fact]
-        public unsafe void ConvertKnownI420ToBGRTest()
+         public unsafe void ConvertKnownI420ToBGRTest()
         {
             int width = 640;
             int height = 480;
 
             byte[] i420 = File.ReadAllBytes("img/ref-i420.yuv");
-            byte[] bgr = PixelConverter.I420toBGR(i420, width, height);
+            byte[] bgr = PixelConverter.I420toBGR(i420, width, height, out int rtStride);
 
             fixed (byte* s = bgr)
             {
-                Bitmap bmp = new Bitmap(width,height, width * 3, PixelFormat.Format24bppRgb, (IntPtr)s);
+                Bitmap bmp = new Bitmap(width,height, rtStride, PixelFormat.Format24bppRgb, (IntPtr)s);
                 bmp.Save("ConvertKnownI420ToBGRTest.bmp");
                 bmp.Dispose();
             }
@@ -100,15 +100,166 @@ namespace SIPSorceryMedia.Abstractions.UnitTest
                 bmp.Dispose();
             }
         }
+          
+        /// <summary>
+        /// Tests that an I420 buffer with an odd height (as in height % 2 != 0) can be converted to BGR.
+        /// </summary>
+        [Fact]
+        public unsafe void ConvertOddDimensionI420ToBGRTest()
+        {
+            int width = 4;
+            int height = 3;
 
-        public static byte[] BitmapToBuffer(Bitmap bitmap)
+            byte[] i420 = new byte[20];
+            byte[] bgr = PixelConverter.I420toBGR(i420, width, height, out _);
+
+            Assert.NotNull(bgr);
+            Assert.Equal(36, bgr.Length);
+        }
+
+        /// <summary>
+        /// Tests that an I420 buffer with less than the required data throws an exception.
+        /// </summary>
+        [Fact]
+        public unsafe void WrongSizeI420ToBGRTest()
+        {
+            int width = 720;
+            int height = 405;
+
+            byte[] i420 = new byte[width * height * 3/2];
+            Assert.Throws<ApplicationException>(() => PixelConverter.I420toBGR(i420, width, height, out _));
+        }
+
+        /// <summary>
+        /// Tests that a 480p image can be converted to I420 and back successfully.
+        /// </summary>
+        [Fact]
+        public unsafe void Roundtrip_Bitmap_640x480()
+        {
+            int width = 640;
+            int height = 480;
+
+            using (Bitmap bmp = new Bitmap($"img/testpattern_{width}x{height}.bmp"))
+            {
+                byte[] bgr = BitmapToBuffer(bmp, out int stride);
+
+                byte[] i420 = PixelConverter.BGRtoI420(bgr, width, height, stride);
+
+                Assert.NotNull(i420);
+
+                byte[] rtBgr = PixelConverter.I420toBGR(i420, width, height, out int rtStride);
+
+                Assert.NotNull(rtBgr);
+
+                fixed (byte* pBgr = rtBgr)
+                {
+                    Bitmap rtBmp = new Bitmap(width, height, rtStride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr(pBgr));
+                    rtBmp.Save($"roundtrip_bitmap_{width}x{height}.bmp");
+                    rtBmp.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests that an image with an uneven height can be converted to I420 and back successfully.
+        /// </summary>
+        [Fact]
+        public unsafe void Roundtrip_Bitmap_720x405()
+        {
+            int width = 720;
+            int height = 405;
+
+            using (Bitmap bmp = new Bitmap($"img/testpattern_{width}x{height}.bmp"))
+            {
+                byte[] bgr = BitmapToBuffer(bmp, out int stride);
+
+                byte[] i420 = PixelConverter.BGRtoI420(bgr, width, height, stride);
+
+                Assert.NotNull(i420);
+
+                byte[] rtBgr = PixelConverter.I420toBGR(i420, width, height, out int rtStride);
+
+                Assert.NotNull(rtBgr);
+                Assert.Equal(874800, rtBgr.Length);
+
+                fixed (byte* pBgr = rtBgr)
+                {
+                    Bitmap rtBmp = new Bitmap(width, height, rtStride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr(pBgr));
+                    rtBmp.Save($"roundtrip_bitmap_{width}x{height}.bmp");
+                    rtBmp.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests that an image with an uneven width can be converted to I420 and back successfully.
+        /// </summary>
+        [Fact]
+        public unsafe void Roundtrip_Bitmap_719x404()
+        {
+            int width = 719;
+            int height = 404;
+
+            using (Bitmap bmp = new Bitmap($"img/testpattern_{width}x{height}.bmp"))
+            {
+                byte[] bgr = BitmapToBuffer(bmp, out int stride);
+
+                byte[] i420 = PixelConverter.BGRtoI420(bgr, width, height, stride);
+
+                Assert.NotNull(i420);
+
+                byte[] rtBgr = PixelConverter.I420toBGR(i420, width, height, out int rtStride);
+
+                Assert.NotNull(rtBgr);
+
+                fixed (byte* pBgr = rtBgr)
+                {
+                    Bitmap rtBmp = new Bitmap(width, height, rtStride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr(pBgr));
+                    rtBmp.Save($"roundtrip_bitmap_{width}x{height}.bmp");
+                    rtBmp.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests that an image with an uneven width and height can be converted to I420 and back successfully.
+        /// </summary>
+        [Fact]
+        public unsafe void Roundtrip_Bitmap_719x405()
+        {
+            int width = 719;
+            int height = 405;
+
+            using (Bitmap bmp = new Bitmap($"img/testpattern_{width}x{height}.bmp"))
+            {
+                byte[] bgr = BitmapToBuffer(bmp, out int stride);
+
+                byte[] i420 = PixelConverter.BGRtoI420(bgr, width, height, stride);
+
+                Assert.NotNull(i420);
+
+                byte[] rtBgr = PixelConverter.I420toBGR(i420, width, height, out int rtStride);
+
+                Assert.NotNull(rtBgr);
+
+                fixed (byte* pBgr = rtBgr)
+                {
+                    Bitmap rtBmp = new Bitmap(width, height, rtStride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr(pBgr));
+                    rtBmp.Save($"roundtrip_bitmap_{width}x{height}.bmp");
+                    rtBmp.Dispose();
+                }
+            }
+        }
+
+        private static byte[] BitmapToBuffer(Bitmap bitmap, out int stride)
         {
             BitmapData bmpdata = null;
 
             try
             {
                 bmpdata = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
-                int numbytes = bmpdata.Stride * bitmap.Height;
+                stride = bmpdata.Stride;
+                int numbytes = stride * bitmap.Height;
                 byte[] bytedata = new byte[numbytes];
                 IntPtr ptr = bmpdata.Scan0;
 
