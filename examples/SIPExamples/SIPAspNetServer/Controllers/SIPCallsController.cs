8bf3781e5c16bff31e45820585229c8e6f1c1d00
Aaron Clauson <aaron@sipsorcery.com>
2021-01-02 21:39:23 +0000

    Added web API controllers for CDRs and calls.

---
+++ examples/SIPExamples/SIPAspNetServer/Controllers/SIPCallsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using demo.DataAccess;
+
+namespace SIPAspNetServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SIPCallsController : ControllerBase
+    {
+        private readonly SIPAssetsDbContext _context;
+
+        public SIPCallsController(SIPAssetsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SIPCalls
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SIPCall>>> GetSIPCalls()
+        {
+            return await _context.SIPCalls.ToListAsync();
+        }
+
+        // GET: api/SIPCalls/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SIPCall>> GetSIPCall(Guid id)
+        {
+            var sIPCall = await _context.SIPCalls.FindAsync(id);
+
+            if (sIPCall == null)
+            {
+                return NotFound();
+            }
+
+            return sIPCall;
+        }
+
+        // PUT: api/SIPCalls/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSIPCall(Guid id, SIPCall sIPCall)
+        {
+            if (id != sIPCall.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sIPCall).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SIPCallExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SIPCalls
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SIPCall>> PostSIPCall(SIPCall sIPCall)
+        {
+            _context.SIPCalls.Add(sIPCall);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SIPCallExists(sIPCall.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSIPCall", new { id = sIPCall.ID }, sIPCall);
+        }
+
+        // DELETE: api/SIPCalls/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSIPCall(Guid id)
+        {
+            var sIPCall = await _context.SIPCalls.FindAsync(id);
+            if (sIPCall == null)
+            {
+                return NotFound();
+            }
+
+            _context.SIPCalls.Remove(sIPCall);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SIPCallExists(Guid id)
+        {
+            return _context.SIPCalls.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ examples/SIPExamples/SIPAspNetServer/README.Testing.txt
@@ -0,0 +1,17 @@
+==> Sends an OPTIONS request.
+dotnet run -- -d sip:172.29.207.116
+
+==> Initiates a call and hangups if/when answered. Good for checking CDRs are generated correctly.
+dotnet run -- -d sip:123@172.29.207.116 -s uac
+
+==> Sends a registration request. Uses user/password for credentials.
+dotnet run -- -d sip:192.168.0.50 -s reg
+insert into sipaccounts values (newid(), '899e48ef-1267-4b53-8a1c-476a176a4e80', 'user', 'password', '0', default);
+
+==> Web API URLs
+https://localhost:5001/swagger/index.html
+https://localhost:5001/api/sipaccounts
+https://localhost:5001/api/sipdomains
+https://localhost:5001/api/sipregistrarbindings
+https://localhost:5001/api/sipcalls
+https://localhost:5001/api/cdrs
