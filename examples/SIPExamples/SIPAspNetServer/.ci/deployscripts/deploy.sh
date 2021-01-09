67fad2880047fb6cf79584e768f5276ec03626f0
Aaron Clauson <aaron@sipsorcery.com>
2021-01-09 12:22:06 +0000

    Updated azure dpeloy script for new path.

--- examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
+++ examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
@@ -1,5 +1,5 @@
 echo 'SIP demo server release deploy script starting...'
 sudo systemctl stop sipdemo
-sudo cp -r _sipsorcery.sipdemo/drop/* /opt/sipdemo
+sudo cp -r '_sipdemo publish/drop/*' /opt/sipdemo
 sudo systemctl start sipdemo
 echo 'SIP demo server release deploy script finished.'
