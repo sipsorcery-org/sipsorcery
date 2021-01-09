aed886f73231da8910c15572d07f370b9e04cc79
Aaron Clauson <aaron@sipsorcery.com>
2021-01-09 14:13:56 +0000

    Removed space in pipeline path to avoid bash script complications.

--- examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
+++ examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
@@ -1,14 +1,10 @@
 echo 'SIP demo server release deploy script starting...'
 pwd
-export
 
-echo "Agent.BuildDirectory=$Agent_BuildDirectory"
-echo "Build.Repository.LocalPath=$Build_Repository_LocalPath"
-echo "Environment.ResourceName=$Environment_ResourceName"
-echo "Environment.ResourceId=$Environment_ResourceId"
-echo "System.DefaultWorkingDirectory=$System_DefaultWorkingDirectory"
+echo "AGENT_RELEASEDIRECTORY=$AGENT_RELEASEDIRECTORY"
+echo "BUILD_DEFINITIONNAME=$BUILD_DEFINITIONNAME"
 
 sudo systemctl stop sipdemo
-sudo cp -r  drop/* /opt/sipdemo
+sudo cp -r "$BUILD_DEFINITIONNAME"/drop/* /opt/sipdemo
 sudo systemctl start sipdemo
 echo 'SIP demo server release deploy script finished.'
