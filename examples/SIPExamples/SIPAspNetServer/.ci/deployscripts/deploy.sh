2cc3a969b11c7218a4030d248f57d6f81dc2e5ca
Aaron Clauson <aaron@sipsorcery.com>
2021-01-09 13:42:15 +0000

    EOL conversion for Azure deploy script.

--- examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
+++ examples/SIPExamples/SIPAspNetServer/.ci/deployscripts/deploy.sh
@@ -2,13 +2,13 @@ echo 'SIP demo server release deploy script starting...'
 pwd
 export
 
-echo "Agent.BuildDirectory=$Agent.BuildDirectory"
-echo "Build.Repository.LocalPath=$Build.Repository.LocalPath"
-echo "Environment.ResourceName=$Environment.ResourceName"
-echo "Environment.ResourceId=$Environment.ResourceId"
-echo "System.DefaultWorkingDirectory=$System.DefaultWorkingDirectory"
+echo "Agent.BuildDirectory=$Agent_BuildDirectory"
+echo "Build.Repository.LocalPath=$Build_Repository_LocalPath"
+echo "Environment.ResourceName=$Environment_ResourceName"
+echo "Environment.ResourceId=$Environment_ResourceId"
+echo "System.DefaultWorkingDirectory=$System_DefaultWorkingDirectory"
 
 sudo systemctl stop sipdemo
-sudo cp -r '_sipdemo publish/drop/*' /opt/sipdemo
+sudo cp -r  drop/* /opt/sipdemo
 sudo systemctl start sipdemo
 echo 'SIP demo server release deploy script finished.'
