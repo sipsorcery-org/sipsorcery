dc30b28b9735af4f0ce08115d401fdc9eeb0b695
Aaron Clauson <aaron@sipsorcery.com>
2021-07-15 15:51:00 +0100

    Fixed bug in WebRTC peer connection logic where DTLS role was staying as passive even if remote party indicated they wanted to be passive. Partially fixes #517.

--- src/net/ICE/IRTCIceCandidate.cs
+++ src/net/ICE/IRTCIceCandidate.cs
@@ -16,6 +16,17 @@
 namespace SIPSorcery.Net
 {
     /// <summary>
+    /// The ICE set up roles that a peer can be in. The role determines how the DTLS
+    /// handshake is performed, i.e. which peer is the client and which is the server.
+    /// </summary>
+    public enum IceRolesEnum
+    {
+        actpass = 0,
+        passive = 1,
+        active = 2
+    }
+
+    /// <summary>
     /// The gathering states an ICE session transitions through.
     /// </summary>
     /// <remarks>
