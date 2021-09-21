c0ef0ac0afe8975657beaad5878540ef462f82a0
Aaron Clauson <aaron@sipsorcery.com>
2021-09-21 16:23:07 +0100

    Make SIPTransport SIP channel dictionary concurrent. Resolves #575.

--- src/core/SIP/SIPTransport.cs
+++ src/core/SIP/SIPTransport.cs
@@ -76,7 +76,7 @@ namespace SIPSorcery.SIP
         /// List of the SIP channels that have been opened and are under management by this instance.
         /// The dictionary key is channel ID (previously was a serialised SIP end point).
         /// </summary>
-        private Dictionary<string, SIPChannel> m_sipChannels = new Dictionary<string, SIPChannel>();
+        private ConcurrentDictionary<string, SIPChannel> m_sipChannels = new ConcurrentDictionary<string, SIPChannel>();
 
         internal SIPTransactionEngine m_transactionEngine;
 
@@ -245,16 +245,21 @@ namespace SIPSorcery.SIP
         {
             try
             {
-                m_sipChannels.Add(sipChannel.ID, sipChannel);
-
-                // Wire up the SIP transport to the SIP channel.
-                sipChannel.SIPMessageReceived += ReceiveMessage;
-
-                if (m_queueIncoming && !m_transportThreadStarted)
+                if (m_sipChannels.TryAdd(sipChannel.ID, sipChannel))
                 {
-                    // Starts tasks to process queued SIP messages.
-                    m_transportThreadStarted = true;
-                    Task.Factory.StartNew(ProcessReceiveQueue, TaskCreationOptions.LongRunning);
+                    // Wire up the SIP transport to the SIP channel.
+                    sipChannel.SIPMessageReceived += ReceiveMessage;
+
+                    if (m_queueIncoming && !m_transportThreadStarted)
+                    {
+                        // Starts tasks to process queued SIP messages.
+                        m_transportThreadStarted = true;
+                        Task.Factory.StartNew(ProcessReceiveQueue, TaskCreationOptions.LongRunning);
+                    }
+                }
+                else
+                {
+                    throw new ApplicationException("Failed to add SIPChannel to the SIP transport.");
                 }
             }
             catch (Exception excp)
@@ -272,7 +277,7 @@ namespace SIPSorcery.SIP
         {
             if (m_sipChannels.ContainsKey(sipChannel.ID))
             {
-                m_sipChannels.Remove(sipChannel.ID);
+                m_sipChannels.TryRemove(sipChannel.ID, out _);
                 sipChannel.SIPMessageReceived -= ReceiveMessage;
             }
         }
