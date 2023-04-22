84377c17c07a44aec70fc37415a4855a58b1594f
Dmitry Petrov <46092536+Dmise@users.noreply.github.com>
2023-04-22 20:54:51 +0300

    Add example RtspToWebRTCAudioAndVideo (#922)

---
+++ examples/WebRTCExamples/RtspToWebRTCAudioAndVideo/StreamsEnum.cs
@@ -0,0 +1,18 @@
+﻿using SIPSorceryMedia.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtspToWebRtcRestreamer
+{
+    internal enum StreamsEnum
+    {
+        video,
+        audio,
+        videoAndAudio,
+        videoAndAudioUdp,
+        none
+    }
+}
