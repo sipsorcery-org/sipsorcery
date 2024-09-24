8fa456aa59a4b8bdbbed9eb21dc2291ee7f2aeab
Aaron Clauson <aaron@sipsorcery.com>
2024-09-24 22:01:17 +0100

    Temporarily disable deprecated ffmpeg channel layout.

--- src/FFmpegAudioDecoder.cs
+++ src/FFmpegAudioDecoder.cs
@@ -136,16 +136,19 @@ namespace SIPSorceryMedia.FFmpeg
                 ffmpeg.av_opt_set_int(_swrContext, "out_sample_rate", clockRate, 0);
 
                 //FIX:Some Codec's Context Information is missing
-                if (_audDecCtx->channel_layout == 0)
-                {
-                    long in_channel_layout = ffmpeg.av_get_default_channel_layout(_audDecCtx->channels);
-                    ffmpeg.av_opt_set_channel_layout(_swrContext, "in_channel_layout", in_channel_layout, 0);
-                }
-                else
-                    ffmpeg.av_opt_set_channel_layout(_swrContext, "in_channel_layout", (long)_audDecCtx->channel_layout, 0);
+                // TODO: Seems like this ffmpeg preopty has now changed to ch_layout. AC 24 Sep 2024.
+                //if (_audDecCtx->channel_layout == 0)
+                //{
+                //    long in_channel_layout = ffmpeg.av_get_default_channel_layout(_audDecCtx->channels);
+                //    ffmpeg.av_opt_set_channel_layout(_swrContext, "in_channel_layout", in_channel_layout, 0);
+                //}
+                //else
+                //    ffmpeg.av_opt_set_channel_layout(_swrContext, "in_channel_layout", (long)_audDecCtx->channel_layout, 0);
 
                 OutChannelCount = 1;
-                ffmpeg.av_opt_set_channel_layout(_swrContext, "out_channel_layout", (long)ffmpeg.AV_CH_LAYOUT_MONO, 0);
+
+                // TODO: Seems like this ffmpeg preopty has now changed to ch_layout. AC 24 Sep 2024
+                //ffmpeg.av_opt_set_channel_layout(_swrContext, "out_channel_layout", (long)ffmpeg.AV_CH_LAYOUT_MONO, 0);
 
                 if ( ffmpeg.swr_init(_swrContext) < 0 )
                 {
