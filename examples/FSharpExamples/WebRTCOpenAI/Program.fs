baf626719d3b50e2eeef72b464908e832d29dd84
Aaron Clauson <aaron@sipsorcery.com>
2025-01-27 22:40:48 +0000

    Slow progress.

--- examples/FSharpExamples/WebRTCOpenAI/Program.fs
+++ examples/FSharpExamples/WebRTCOpenAI/Program.fs
@@ -43,15 +43,32 @@
 //-----------------------------------------------------------------------------
 
 open demo
+open Microsoft.Extensions.Logging
 open Microsoft.FSharp.Core
+open Serilog
 open System
 open System.Threading
+open SIPSorcery.Media
+open SIPSorcery.Net
+open SIPSorceryMedia.Windows
 
 let OPENAI_REALTIME_SESSIONS_URL = "https://api.openai.com/v1/realtime/sessions"
 let OPENAI_REALTIME_BASE_URL = "https://api.openai.com/v1/realtime"
 let OPENAI_MODEL = "gpt-4o-realtime-preview-2024-12-17"
 let OPENAI_VOICE = OpenAIVoicesEnum.shimmer
 let OPENAI_DATACHANNEL_NAME = "oai-events"
+
+/// Initialize the logger
+let logger =
+    LoggerFactory.Create(fun builder ->
+        builder.AddSerilog(
+            LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger()
+        ) |> ignore
+    ).CreateLogger("demo")
 
 /// Convert Either<LError, string> to F# Result<string, string> by storing only error messages
 let eitherToFSharpResult (either: LanguageExt.Either<LanguageExt.Common.Error, string>) : Result<string, string> =
@@ -84,6 +101,74 @@ let getEphemeralKey (url: string) (openAIKey: string) (model: string) (voice: Op
                 printfn "Failed to obtain ephemeral key: %s" error
                 return None
         }
+
+let OnDataChannelMessage (logger: ILogger) (dc: RTCDataChannel) (protocol: DataChannelPayloadProtocols) (data: byte[]) =
+    match protocol with
+    | DataChannelPayloadProtocols.WebRTC_Binary ->
+        logger.Debug($"Data channel {dc.label} received binary message of length {data.Length}.")
+    | DataChannelPayloadProtocols.WebRTC_String ->
+        let message = System.Text.Encoding.UTF8.GetString(data)
+        logger.Debug($"Data channel {dc.label} received text message {message}.")
+    | _ -> ()
+
+/// <summary>
+/// Method to create the local peer connection instance and data channel.
+/// </summary>
+/// <param name="onConnectedSemaphore">A semaphore that will get set when the data channel on the peer connection is opened. Since the data channel
+/// can only be opened once the peer connection is open this indicates both are ready for use.</param>
+let createPeerConnection (logger: ILogger) (onConnectedSemaphore: SemaphoreSlim) : Async<RTCPeerConnection> = async {
+    let pcConfig = RTCConfiguration(X_UseRtpFeedbackProfile = true)
+    let peerConnection = new RTCPeerConnection(pcConfig)
+    let! dataChannel = peerConnection.createDataChannel(OPENAI_DATACHANNEL_NAME) |> Async.AwaitTask
+
+    // Sink (speaker) only audio end point.
+    let windowsAudioEP = WindowsAudioEndPoint(AudioEncoder(includeOpus = true), -1, -1, false, false)
+    windowsAudioEP.RestrictFormats(fun x -> x.FormatName = "OPUS")
+    windowsAudioEP.add_OnAudioSinkError(fun err -> logger.Warning($"Audio sink error. {err}."))
+    windowsAudioEP.add_OnAudioSourceEncodedSample(fun duration sample -> peerConnection.SendAudio(duration, sample))
+
+    let audioTrack = MediaStreamTrack(windowsAudioEP.GetAudioSourceFormats(), MediaStreamStatusEnum.SendRecv)
+    peerConnection.addTrack(audioTrack)
+
+    peerConnection.add_OnAudioFormatsNegotiated(fun audioFormats ->
+        logger.Debug($"Audio format negotiated {audioFormats.Head().FormatName}.")
+        windowsAudioEP.SetAudioSinkFormat(audioFormats.Head())
+        windowsAudioEP.SetAudioSourceFormat(audioFormats.Head())
+    )
+
+    peerConnection.add_OnTimeout(fun mediaType -> logger.Debug($"Timeout on media {mediaType}."))
+    peerConnection.add_oniceconnectionstatechange(fun state -> logger.Debug($"ICE connection state changed to {state}."))
+    peerConnection.add_onconnectionstatechange(fun state -> 
+        Async.Start(
+            async {
+                logger.Debug($"Peer connection state changed to {state}.")
+
+                match state with
+                | RTCPeerConnectionState.connected ->
+                    do! windowsAudioEP.StartAudio() |> Async.AwaitTask
+                    do! windowsAudioEP.StartAudioSink() |> Async.AwaitTask
+                | RTCPeerConnectionState.closed | RTCPeerConnectionState.failed ->
+                    do! windowsAudioEP.CloseAudio() |> Async.AwaitTask
+                | _ -> ()
+            }
+        )
+    )
+
+    peerConnection.add_OnRtpPacketReceived(fun rep media rtpPkt ->
+        if media = SDPMediaTypesEnum.audio then
+            windowsAudioEP.GotAudioRtp(rep, rtpPkt.Header.SyncSource, uint32 rtpPkt.Header.SequenceNumber, rtpPkt.Header.Timestamp, rtpPkt.Header.PayloadType, rtpPkt.Header.MarkerBit = 1, rtpPkt.Payload)
+    )
+
+    dataChannel.add_onopen(fun () ->
+        logger.Debug("OpenAI data channel opened.")
+        onConnectedSemaphore.Release() |> ignore
+    )
+
+    dataChannel.add_onclose(fun () -> logger.Debug($"OpenAI data channel {dataChannel.label} closed."))
+    dataChannel.add_onmessage(fun dc protocol data -> OnDataChannelMessage logger dc protocol data)
+
+    return peerConnection
+}
 
 [<EntryPoint>]
 let main argv =
